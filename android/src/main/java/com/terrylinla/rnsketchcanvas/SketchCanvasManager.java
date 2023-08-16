ea48116affb51a3035c509816a8361dde662ac86
Connor Tumbleson <connor@sourcetoad.com>
2023-08-16 09:27:36 -0400

    style: lint Android code + remove unused imports

--- android/src/main/java/com/terrylinla/rnsketchcanvas/SketchCanvasManager.java
+++ android/src/main/java/com/terrylinla/rnsketchcanvas/SketchCanvasManager.java
@@ -1,22 +1,7 @@
 package com.terrylinla.rnsketchcanvas;
 
-import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
-import android.util.Log;
-
-import com.facebook.common.logging.FLog;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.common.ReactConstants;
-import com.facebook.react.common.MapBuilder;
-import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
@@ -24,6 +9,7 @@ import com.facebook.react.uimanager.annotations.ReactProp;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
+
 import android.graphics.PointF;
 
 import javax.annotation.Nullable;
@@ -36,6 +22,7 @@ public class SketchCanvasManager extends SimpleViewManager<SketchCanvas> {
     public static final int COMMAND_DELETE_PATH = 5;
     public static final int COMMAND_SAVE = 6;
     public static final int COMMAND_END_PATH = 7;
+    public static final int COMMAND_TO_BASE64 = 8;
 
     public static SketchCanvas Canvas = null;
 
@@ -57,9 +44,9 @@ public class SketchCanvasManager extends SimpleViewManager<SketchCanvas> {
     public void setLocalSourceImage(SketchCanvas viewContainer, ReadableMap localSourceImage) {
         if (localSourceImage != null && localSourceImage.getString("filename") != null) {
             viewContainer.openImageFile(
-                localSourceImage.hasKey("filename") ? localSourceImage.getString("filename") : null,
-                localSourceImage.hasKey("directory") ? localSourceImage.getString("directory") : "",
-                localSourceImage.hasKey("mode") ? localSourceImage.getString("mode") : ""
+                    localSourceImage.hasKey("filename") ? localSourceImage.getString("filename") : null,
+                    localSourceImage.hasKey("directory") ? localSourceImage.getString("directory") : "",
+                    localSourceImage.hasKey("mode") ? localSourceImage.getString("mode") : ""
             );
         }
     }
@@ -70,7 +57,7 @@ public class SketchCanvasManager extends SimpleViewManager<SketchCanvas> {
     }
 
     @Override
-    public Map<String,Integer> getCommandsMap() {
+    public Map<String, Integer> getCommandsMap() {
         Map<String, Integer> map = new HashMap<>();
 
         map.put("addPoint", COMMAND_ADD_POINT);
@@ -90,14 +77,19 @@ public class SketchCanvasManager extends SimpleViewManager<SketchCanvas> {
     }
 
     @Override
+    public void receiveCommand(SketchCanvas view, String commandType, @Nullable ReadableArray args) {
+        receiveCommand(view, Integer.parseInt(commandType), args);
+    }
+
+    @Override
     public void receiveCommand(SketchCanvas view, int commandType, @Nullable ReadableArray args) {
         switch (commandType) {
             case COMMAND_ADD_POINT: {
-                view.addPoint((float)args.getDouble(0), (float)args.getDouble(1));
+                view.addPoint((float) args.getDouble(0), (float) args.getDouble(1));
                 return;
             }
             case COMMAND_NEW_PATH: {
-                view.newPath(args.getInt(0), args.getInt(1), (float)args.getDouble(2));
+                view.newPath(args.getInt(0), args.getInt(1), (float) args.getDouble(2));
                 return;
             }
             case COMMAND_CLEAR: {
@@ -107,11 +99,11 @@ public class SketchCanvasManager extends SimpleViewManager<SketchCanvas> {
             case COMMAND_ADD_PATH: {
                 ReadableArray path = args.getArray(3);
                 ArrayList<PointF> pointPath = new ArrayList<PointF>(path.size());
-                for (int i=0; i<path.size(); i++) {
+                for (int i = 0; i < path.size(); i++) {
                     String[] coor = path.getString(i).split(",");
                     pointPath.add(new PointF(Float.parseFloat(coor[0]), Float.parseFloat(coor[1])));
                 }
-                view.addPath(args.getInt(0), args.getInt(1), (float)args.getDouble(2), pointPath);
+                view.addPath(args.getInt(0), args.getInt(1), (float) args.getDouble(2), pointPath);
                 return;
             }
             case COMMAND_DELETE_PATH: {
@@ -126,6 +118,7 @@ public class SketchCanvasManager extends SimpleViewManager<SketchCanvas> {
                 view.end();
                 return;
             }
+
             default:
                 throw new IllegalArgumentException(String.format(
                         "Unsupported command %d received by %s.",
