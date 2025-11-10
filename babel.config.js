f0d9cafdde7d9f1719585533ac6d25083c6989e7
Truitt Janney <truitt.janney@sourcetoad.com>
2025-11-10 13:34:04 -0600

    babel format issue fix

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,20 @@
 module.exports = {
   presets: [
     'module:react-native-builder-bob/babel-preset',
-    ['@babel/preset-typescript', { allowNamespaces: true, allowDeclareFields: true }],
+    [
+      '@babel/preset-typescript',
+      { allowNamespaces: true, allowDeclareFields: true },
+    ],
     '@babel/preset-flow',
   ],
   env: {
     test: {
       presets: [
         ['@react-native/babel-preset'],
-        ['@babel/preset-typescript', { allowNamespaces: true, allowDeclareFields: true }],
+        [
+          '@babel/preset-typescript',
+          { allowNamespaces: true, allowDeclareFields: true },
+        ],
         '@babel/preset-flow',
       ],
     },
