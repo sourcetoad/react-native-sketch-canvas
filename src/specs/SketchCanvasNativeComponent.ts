f35727327d54d1275e276f64dc97201cf5bd0c9f
Connor Tumbleson <connor@sourcetoad.com>
2026-03-18 10:59:44 -0400

    fix: prevent deep loading on react-native

--- src/specs/SketchCanvasNativeComponent.ts
+++ src/specs/SketchCanvasNativeComponent.ts
@@ -1,6 +1,5 @@
 import type { ViewProps, HostComponent } from 'react-native';
-import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
-import codegenNativeCommands from 'react-native/Libraries/Utilities/codegenNativeCommands';
+import { codegenNativeComponent, codegenNativeCommands } from 'react-native';
 import type {
   DirectEventHandler,
   BubblingEventHandler,
